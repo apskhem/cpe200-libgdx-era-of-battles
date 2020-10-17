178dc0ac8088f203f7979a768de62c1f60d85433
Apisit Ritreungroj <apisit_lon@hotmail.com>
2020-10-17 12:37:58 +0700

    Minor Ultimate Update

--- core/src/coma/game/Ultimate.java
+++ core/src/coma/game/Ultimate.java
@@ -7,17 +7,13 @@ public class Ultimate {
     public final ArrayList<UltimateObj> ultimateContainer = new ArrayList<>();
 
     //constant
-    private static short ULTIMATE_SPAWN_POS_Y = 600;
-    private static short ULTIMATE_EXPLODE_POS_Y = 0;
-    private static short ULTIMATE_SPAWN_POS_XMIN = 0;
-    private static short ULTIMATE_SPAWN_POS_XMAX = 960;
+    public static short SPAWN_POS_Y = 600;
+    public static short EXPLODE_POS_Y = 0;
+    public static short SPAWN_POS_XMIN = 0;
+    public static short SPAWN_POS_XMAX = 960;
 
     public Player target;
     public Player caller;
-    public final float speedY;
-    public final float speedX;
-    public float x;
-    public float y;
 
     public Ultimate() {
         int maxSize = Mathf.CalRange(10,15);
@@ -30,13 +26,13 @@ public class Ultimate {
     }
 
     public void ContinueUltimate() {
-        if (this.target == null) return;
+        if (this.target == null && this.caller == null) return;
 
         final ArrayList<UltimateObj> explodedUltimateObjects = new ArrayList<>();
 
         // new distance calculation and checking explosion
         for (final UltimateObj obj : this.ultimateContainer) {
-            if (this.y < 0) {
+            if (obj.y < Ultimate.EXPLODE_POS_Y) {
                 obj.ultimateExplode();
 
                 //++ DO SOMETHING WITH TARGET
@@ -44,8 +40,7 @@ public class Ultimate {
                 explodedUltimateObjects.add(obj);
             }
             else {
-                this.x += this.speedX;
-                this.y += this.speedY;
+                obj.ultimateMove();
             }
         }
 
@@ -60,25 +55,26 @@ public class Ultimate {
 class UltimateObj extends GameObject{
 
     //constant
-    private final short vectorY = -600;
+    public final float speedX;
+    public final float speedY;
+    public float x;
+    public float y = Ultimate.SPAWN_POS_Y;
+    public short damage;
+    public short era;
 
-    short damage;
-
-    public UltimateObj(byte era){
+    public UltimateObj(byte era) {
         super(MainGame.ultimateImages[era].Clone());
     }
 
-    public void ultimateMove(){
+    public void ultimateMove() {
 
     }
 
-    public void ultimateExplode(){
-        if (vectorY == 0){
-            //explode & send damage to in-area opponent troops
-            //era 1 explode
-            //era 2 more arrow
-            //era 3 explode
-            //era 4 laser beam
-        }
+    public void ultimateExplode() {
+        //explode & send damage to in-area opponent troops
+        //era 1 explode
+        //era 2 more arrow
+        //era 3 explode
+        //era 4 laser beam
     }
 }
